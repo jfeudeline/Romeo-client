e4a7a146cce717ea72057409bfa6166b1dd9e643
Jean-François Eudeline <jfeudeline@gmail.com>
2024-10-25 14:50:45 +0200

    Page Home

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,14 @@
-import type { Metadata } from "next";
-import "@/app/ui/globals.css";
-import { inter } from "@/app/ui/fonts";
+import type { Metadata } from 'next';
+import '@/app/ui/globals.css';
+import { inter } from '@/app/ui/fonts';
+import SideNav from '@/app/ui/sidenav';
 
 export const metadata: Metadata = {
   title: {
-    template: "%s | Romeo Client",
-    default: "Romeo Client",
+    template: '%s | Romeo Client',
+    default: 'Romeo Client',
   },
-  description: "Aplication cliente des API Rome et Romeo de France Travail ",
+  description: 'Aplication cliente des API Rome et Romeo de France Travail ',
 };
 
 export default function RootLayout({
@@ -17,7 +18,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+          <div className="w-full flex-none md:w-64">
+            <SideNav />
+          </div>
+          <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
+            {children}
+          </div>
+        </div>
+      </body>
     </html>
   );
 }
