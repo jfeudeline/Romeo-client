0ea79e8b7eb91b827fa580733d0bb6ea6430f7fb
Jean-François Eudeline <jfeudeline@gmail.com>
2024-10-30 09:36:01 +0100

    fix eslint

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,37 +3,30 @@ import { lusitana } from '@/app/ui/fonts';
 import { TableFichesDT } from '@/app/ui/home/table';
 import { getFichesMetier } from '../lib/api-requests';
 
-
 interface FicheMetier {
- code: string,
- metier: {
-  code:string,
-  libelle:string,
- }
+  code: string;
+  metier: {
+    code: string;
+    libelle: string;
+  };
 }
 
 export default async function Home() {
-
   const fichesMetier = await getFichesMetier();
 
-  
   const fiches = fichesMetier.map((fiche: FicheMetier) => {
     return {
       code: fiche.metier.code,
       libelle: fiche.metier.libelle,
-    }
+    };
+  });
 
-  })
-  
-
-
-  
   return (
     <main className="flex min-h-screen flex-col p-6">
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Les Métiers du ROME 4.0
       </h1>
-      <h2 className={`${lusitana.className} mb-4 text-l md:text-2xl`}>
+      <h2 className={`${lusitana.className} text-l mb-4 md:text-2xl`}>
         Les fiches métier
       </h2>
       <TableFichesDT fichesMetier={fiches} />
